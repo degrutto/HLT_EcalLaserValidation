6328d681c06c4ccc7fb3162b5bcc8a89acdef9a5
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2017-12-13 10:06:59 +0100

    use cat instead of more

--- steps.sh
+++ steps.sh
@@ -11,7 +11,7 @@ sleep 5
 ###############################
 testMenu=/cdaq/physics/Run2017/2e34/v4.0.1/HLT/V1
 GT=92X_dataRun2_HLT_v7
-file=$(more files_305188.txt)
+file=$(cat files_305188.txt)
 sqlite1=DBLaser_293491
 sqlite2=DBLaser_292925
 pathToMonitor=("HLT_Ele35_WPTight_Gsf" "HLT_PFMET110_PFMHT110_IDTight"  "HLT_Photon33"  "HLT_PFJet450" "HLT_PFMETTypeOne100_PFMHT100_IDTight_PFHT60" "HLT_Ele27_WPTight_Gsf" )
