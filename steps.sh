dffaea562e1331f2f4905199b2128f8bbec2d123
simone <simone.gennai@cern.ch>
2018-01-23 18:57:01 +0100

    changed max number of events to 10k

--- steps.sh
+++ steps.sh
@@ -27,11 +27,11 @@ cd $CMSREL/src
 eval `scram runtime -sh`
 
 
-echo "will run : hltGetConfiguration --offline --globaltag " $GT   "--max-events 999999 --timing  --input  "$file "orcoff:"$testMenu 
+echo "will run : hltGetConfiguration --offline --globaltag " $GT   "--max-events 10000 --timing  --input  "$file "orcoff:"$testMenu 
 #echo "will run : hltGetConfiguration --offline --globaltag auto:run2_hlt_GRun --max-events 999999 --timing  --input  "$file "orcoff:"$testMenu 
 
 
-hltGetConfiguration --online --globaltag $GT   --max-events 99999  --input $(cat files_305183.txt) orcoff:$testMenu > hlt.py
+hltGetConfiguration --online --globaltag $GT   --max-events 100009  --input $(cat files_305183.txt) orcoff:$testMenu > hlt.py
 #hltGetConfiguration --online --globaltag auto:run2_hlt_GRun   --max-events 99999  --input $(cat files_305183.txt) orcoff:$testMenu > hlt.py
 
 cat fastTimeAdd_new.py >> hlt.py
