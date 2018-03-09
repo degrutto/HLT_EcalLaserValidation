4ebf63a7bae5137646017f8db1714775c022e4a9
simone <simone.gennai@cern.ch>
2018-03-09 08:36:45 +0100

    Trying to upload a reference run

--- runHLTEcalLaserValidation.sh
+++ runHLTEcalLaserValidation.sh
@@ -6,8 +6,8 @@ sleep 5
 testMenu=/cdaq/physics/Run2017/2e34/v4.0.1/HLT/V1
 GT=92X_dataRun2_HLT_v7
 file=$(cat files_305188.txt)
-sqlite1=DBLaser_$1
-sqlite2=DBLaser_$2
+reference=$1
+sqlite=DBLaser_$2
 updateNumber=$3
 pathToMonitor=("HLT_Ele3" "HLT_PFMET120_PFMHT120_IDTight"  "HLT_Photon33" "HLT_PFMETTypeOne100_PFMHT100_IDTight_PFHT60" "HLT_Ele27_WPTight_Gsf" )
 maxEvents=100
@@ -26,35 +26,31 @@ eval `scram runtime -sh`
 echo "will run : hltGetConfiguration --offline --globaltag " $GT   "--max-events 999999 --timing  --input  "$file "orcoff:"$testMenu 
 
 
-wget http://cern.ch/ecaltrg/DBLaser/${sqlite1}.db
-#wget http://cern.ch/ecaltrg/DBLaser/${sqlite2}.db
+wget http://cern.ch/ecaltrg/DBLaser/${sqlite}.db
+
 
 hltGetConfiguration --online --globaltag $GT   --max-events $maxEvents  --input $(cat files_305188.txt) orcoff:$testMenu > hlt.py
 
 cat fastTimeAdd_new.py >> hlt.py
 
 
-sed 's/TOADAPT/'$sqlite1'/g' hlt.py  > hlt_sqlite1.py
-#sed 's/TOADAPT/'$sqlite2'/g' hlt.py  > hlt_sqlite2.py
+sed 's/TOADAPT/'$sqlite'/g' hlt.py  > hlt_sqlite.py
 
 
+cmsRun hlt_sqlite.py >&log_sqlite.log 
 
+mkdir ${WORKSPACE}/upload/$2
+cp log_sqlite.log  ${WORKSPACE}/upload/${2}/log_ref_${2}.log 
 
-cmsRun hlt_sqlite1.py >&log_sqlite1.log 
-#cmsRun hlt_sqlite2.py >&log_sqlite2.log 
-
-mkdir ${WORKSPACE}/upload/$1
-cp log_sqlite1.log  ${WORKSPACE}/upload/$1 
-
-#try to retrieve the output from the previous update
+wget  https://cmssdt.cern.ch/SDT/cms-artifacts/EcalLaserValidation/${1}/log_ref_${1}.log 
 
 
 for path in ${pathToMonitor[*]}
 do
    printf "checking for    %s\n" $path
-   cat log_sqlite1.log | grep $path >  $path\_sqlite1.log
-#   cat log_sqlite2.log | grep $path >  $path\_sqlite2.log 
-#   diff $path\_sqlite1.log $path\_sqlite2.log | grep TrigReport >> $path\_diff.log || true
+   cat log_sqlite.log | grep $path >  $path\_sqlite.log
+   cat log_ref_${1}.log | grep $path >  ${path}\_ref_${1}.log 
+   diff $path\_sqlite.log $path\_ref_${1}.log | grep TrigReport >> $path\_diff.log || true
 done
 
 
