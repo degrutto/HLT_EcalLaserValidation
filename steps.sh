7fee7f762f708646dd85f3a79ffab1504ceb1523
simone <simone.gennai@cern.ch>
2018-04-10 11:51:36 +0200

    committing the proper file now...

--- steps.sh
+++ steps.sh
@@ -1,18 +1,20 @@
 #!/bin/bash -ex
-
 file=`ls ToRun/`
-
 echo $file
 if [ -f ToRun/$file ]
 then
 echo ToRun/$file
     year=`grep "year" ToRun/$file | awk '{print $2}'`
     week=`grep "week" ToRun/$file | awk '{print $2}'`
-    sqlite1=`grep "run1" ToRun/$file | awk '{print $2}'`
-    sqlite2=`grep "run2" ToRun/$file | awk '{print $2}'`
-mv ToRun/$file RunFiles/.
-echo "./runHLTEcalLaserValidation.sh $sqlite1 $sqlite2"
-./runHLTEcalLaserValidation.sh $sqlite1 $sqlite2
+    sqliteRef=`grep "run1" ToRun/$file | awk '{print $2}'`
+    sqliteNew=`grep "run2" ToRun/$file | awk '{print $2}'`
+    sqlitePed=`grep "sqlite_pedes" ToRun/$file | awk '{print $2}'`
+cp ToRun/$file RunFiles/.
+rm ToRun/$file
+echo "./runHLTEcalLaserValidation.sh $sqliteRef $sqliteNew $sqlitePed $week"
+./runHLTEcalLaserValidation.sh $sqliteRef $sqliteNew $sqlitePed $week 
+git commit -a -m "clean ToRun files"
+git push
 else
 echo "No new files"
 fi
